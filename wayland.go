8ba3a7b6f6b4828cf4263a759d269219568e3a93
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2016-03-10 15:11:13 +0100

    Implement new wlc api functions related to subsurfaces/wayland

--- wayland.go
+++ wayland.go
@@ -63,6 +63,32 @@ func (v View) GetSurface() Resource {
 	return Resource(C.wlc_view_get_surface(C.wlc_handle(v)))
 }
 
+// GetSubsurfaces returns a list of subsurfaces for a surface.
+func (s Resource) GetSubsurfaces() []Resource {
+	var len C.size_t
+	resouces := C.wlc_surface_get_subsurfaces(C.wlc_resource(s), &len)
+	subsurfaces := make([]Resource, int(len))
+	size := int(unsafe.Sizeof(*resouces))
+	for i := 0; i < int(len); i++ {
+		ptr := unsafe.Pointer(uintptr(unsafe.Pointer(resouces)) + uintptr(size*i))
+		subsurfaces[i] = *(*Resource)(ptr)
+	}
+	return subsurfaces
+}
+
+// GetSubsurfaceGeometry returns the size of a subsurface and its position
+// relative to parent surface.
+func (s Resource) GetSubsurfaceGeometry() Geometry {
+	cgeometry := C.struct_wlc_geometry{}
+	C.wlc_get_subsurface_geometry(C.wlc_resource(s), &cgeometry)
+	return *geometryCtoGo(&Geometry{}, &cgeometry)
+}
+
+// GetWlClient returns wlc_client from view.
+func (v View) GetWlClient() *C.struct_wl_client {
+	return C.wlc_view_get_wl_client(C.wlc_handle(v))
+}
+
 // GetRole returns surface role resource from view handle. Return value
 // will be nil if the view was not assigned role or created with
 // ViewCreateFromSurface().
